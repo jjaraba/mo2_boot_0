4c1a2b66b45a6fa86ab7f7baf3044b6ff6b187e8
Jose Luis Jaraba <jjaraba@hotmail.com>
2019-04-16 20:32:02 +0200

    Ejemplo de objetos con la tortuga

---
+++ objetos.py
@@ -0,0 +1,12 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+otratortuguita = turtle.Turtle()
+
+tortuguita.shape('turtle')
+tortuguita.fd(50)
+
+
+otratortuguita.color('green')
+otratortuguita.left(90)
+otratortuguita.fd(50)
