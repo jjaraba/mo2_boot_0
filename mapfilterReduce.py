20e744c3601e5f2414324cd41d3f52b6a9e7f0a0
Jose Luis Jaraba <jjaraba@hotmail.com>
2019-04-18 20:55:17 +0200

    Reduce primer elemento distinto y indices de lista

--- mapfilterReduce.py
+++ mapfilterReduce.py
@@ -5,6 +5,26 @@ def doble(x):
 
 def esPar(x):
     return x % 2 == 0
+def SumatorioClasico(l):
+    resultado = 0
+    for valor in l:
+        resultado += valor
+        
+    return resultado
+
+def SumatorioDobleClasico(l):
+    resultado = 0
+    for valor in l:
+        resultado += valor *2
+        
+    return resultado
+
+
+def ProductoTotal(l):
+    resultado = 1
+    for valor in l:
+        resultado *= valor
+    return resultado
 
 lista = [1, 3, -1, 15, 9]
 
@@ -16,14 +36,20 @@ listaPares = filter(lambda x: x % 2 ==0, lista)
 listaPares1 =filter(esPar, lista)
 
 sumatorio = reduce(lambda x, y: x + y, lista)
-sumatorioDobles = reduce(lambda x,y: x + y * 2, lista)
+#Creo una copia de la lista
+l=lista[:]
+# Añado el neutro para la sum en la posicion cero
+l.insert(0,0)
+
 suma100 = reduce(lambda x,y: x+y, range(101))
+sumatorioDobles = reduce(lambda x,y: x + y * 2, l)
+
 
 print (list(listaPares))
 print (list(listaPares1))
 
 print (sumatorio)
-print (sumatorioDobles)
+print ("sumatorio doble",sumatorioDobles)
 
 print (suma100)
 
